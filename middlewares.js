1f1a81e611df2ea55d363285809371b1fc6cbb76
Guilherme <guilhermeboavenurarodrigues@gmail.com>
2023-08-30 15:17:44 -0300

    logs

--- middlewares.js
+++ middlewares.js
@@ -1,10 +1,23 @@
 const bodyParser = require("body-parser");
+const cors = require("cors")
+const logger = require('./logger.js')
+const morgan = require("morgan")
 
 module.exports = (app) => {
   app.set("port", 3000);
   app.set("json	spaces", 4);
+  app.use(morgan('common',	{
+    stream:	{
+        write:(log)	=> logger.info(log)
+    }
+  }));
   app.use(bodyParser.json());
   app.use(app.auth.initialize());
+  app.use(cors({
+    origin:[''], //Endereço do endereço que vai ser autorizado a interagir com nossa API
+    methods:['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }))
   app.use((req, res, next) => {
     delete req.body.id;
     next();
