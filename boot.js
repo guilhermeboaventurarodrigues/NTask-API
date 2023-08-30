8b49d6aff621b1efa70acce9d2b0d7053e21eb27
Guilherme <guilhermeboavenurarodrigues@gmail.com>
2023-08-30 15:57:15 -0300

    certificate https

--- boot.js
+++ boot.js
@@ -1,10 +1,19 @@
+const https = require("https")
+const fs = require("fs")
+
+const certs = {
+    key: fs.readFileSync('ntask.pkey', 'utf-8'),
+    cert: fs.readFileSync('ntask.cer', 'utf-8')
+}
+
 module.exports = app => {
    async function start(port) {
     try {
         await app.db.authenticate();
         await app.db.sync();
         if(process.env.NODE_ENV !== 'test'){
-            app.listen(app.get('port'), () => {
+            const server = https.createServer(certs, app)
+            server.listen(app.get('port'), () => {
                 console.log(`NTask API - porta ${app.get('port')}`)
             })
         }
